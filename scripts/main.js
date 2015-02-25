a20f891220548512fd2f87ccfe751043abc16478
Jasper Furniss <jasperfurniss@gmail.com>
2015-02-25 14:20:44 -0500

    final commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,84 @@
 (function() {
-    'use strict';
-    window.App = window.App || {};
+  'use strict';
+  window.App = window.App || {};
 
 
-    //************ MODELS & COLLECTIONS ************//
+  //************ MODELS & COLLECTIONS ************//
 
 
-    var AppModel = Backbone.Model.extend({
-      defaults: {
-        searchTerm: '',
-      }
-    });
+  var AppModel = Backbone.Model.extend({
+    defaults: {
+      searchTerm: '',
+    }
+  });
 
-    var Restaurant = Backbone.Model.extend({});
+  var Restaurant = Backbone.Model.extend({
+    defaults: {
+      searchTerm: '',
+    }
+  });
 
-    var RestaurantCollection = Backbone.Collection.extend({
-        initialize: function(collection, options) {
-          this.appModel = options.appModel;
-        },
+  var RestaurantCollection = Backbone.Collection.extend({
+    initialize: function(collection, options) {
+      this.appModel = options.appModel;
+    },
 
-        url: function() {
-          var searchTerm = this.appModel.get('searchTerm').replace(/ /g, '+');
-          var cors = 'http://jsonp.nodejitsu.com/?url=';
-          var url = 'http://api.yelp.com/v2/search';
-          var location = 'Greenville+South+Carolina',
-            var foodtype = 'find_desc='
+    url: function() {
+      var searchTerm = this.appModel.get('searchTerm').replace(/ /g, '+');
+      var cors = 'http://jsonp.nodejitsu.com/?url=';
+      var base = 'https://api.foursquare.com/v2/venues/search?';
+      var location = '&near=Greenville+SC';
+      var clientid = '&client_id=XH3032NSAMJO2X3FLNDT3NXTILSADCFD2HBPDRI41N5K4SUU';
+      var clientsecret = '&client_secret=ZPZ5YLBVPMBM1RFKH5GE0114BMHPHDUU5F3IJKQLNK3YPV4Z';
+      var date = '&v=20150215';
+      return base + location + searchTerm + clientid + clientsecret + date;
 
-          oauth_consumer_key: 'fENyD3V4qciqnAlyAoBn5A', //Consumer Key
-            oauth_token: 'jd0offZLUxZUESr2FmOzlp6wRRw1AKgW', //Token
-            oauth_signature_method: "HMAC-SHA1",
-            oauth_timestamp: '',
-            oauth_nonce: '',
-        };
+    },
 
-        return cors + encodeURIComponent(url + searchTerm + key);
+    sync: function(method, collection, options) {
+      options.dataType = 'json';
+      Backbone.sync(method, collection, options);
+    },
 
-      },
+    parse: function(response) {
+      return response.results;
+    }
 
-      sync: function(method, collection, options) {
-        options.dataType = 'json';
-        Backbone.sync(method, collection, options);
-      },
-
-      parse: function(response) {
-        return response.results;
-      }
-
-    });
+  });
 
 
 
   //************ VIEWS ************//
 
+
+  //***Index***//
+
+  var IndexView = Backbone.View.extend({
+    tagName: 'form',
+    events: {
+      'submit': 'submitSearch'
+    },
+
+    submitSearch: function() {
+      event.preventDefault();
+      var searchText = $('input').val();
+      var searchTerm = encodeURI(searchText);
+
+      router.navigate('results/' + searchTerm, {
+        trigger: true
+      });
+    },
+
+    template: _.template($('[date-template-name=index]').text()),
+
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    }
+  });
+
+
+  //***Results***//
 
   var ResultsView = Backbone.View.extend({
 
@@ -61,11 +88,13 @@
       this.listenTo(this.collection, 'sync', this.render);
     },
 
+    template: _.template($('[data-template-name=results]').text()),
+
     render: function() {
       this.$el.empty();
       var self = this;
       this.collection.each(function(listing) {
-        self.$el.append('<li>' + listing.get('title') + '</li>');
+        self.$el.append('<span>' + listing.get('name') + '</span>');
       });
     },
   });
